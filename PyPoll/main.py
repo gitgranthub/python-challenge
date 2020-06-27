4044a87d4d2059a1ff15c3ac1126a2a25a22269c
gitgrant <63924944+gitgranthub@users.noreply.github.com>
2020-06-27 18:02:29 -0500

    PyPoll cleanup

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -13,15 +13,10 @@ with open(csvpath) as csvfile:
 
     #variables holding values in the csv file
     
-    khan_pct = 0
     khan_vote_total = 0
-    correy_pct = 0
     correy_vote_total = 0
-    li_pct = 0
     li_vote_total = 0
-    otooley_pct = 0
     otooley_vote_total = 0
-    
     total_votes = 0
     
     for row in csvreader:
@@ -37,8 +32,13 @@ with open(csvpath) as csvfile:
             otooley_vote_total += 1
 
 #Final Statistical Analysis Calculations before print
-winner = "Kahn"
-canidates_totals_list = []
+canidates_totals_dict = {"Kahn": int(khan_vote_total), "Correy": int(correy_vote_total), "Li": int(li_vote_total), "O'Tooley": int(otooley_vote_total)}
+khan_pct = round(khan_vote_total * 100.0 / total_votes)
+correy_pct = round(correy_vote_total * 100.0 / total_votes)
+li_pct = round(li_vote_total * 100.0 / total_votes)
+otooley_pct = round(otooley_vote_total * 100.0 / total_votes)
+winner = max(canidates_totals_dict, key=canidates_totals_dict.get) 
+
 
 #This variable allows 'next line' code to be embeded in this print statement f-string
 nl = "\n" 
@@ -46,20 +46,20 @@ nl = "\n"
 
 print(f"{nl}{nl}Election Results{nl}-------------------------------")
 print(f"Total Votes: {total_votes}{nl}-------------------------------")
-print(f"Kahn: {khan_pct}% ({khan_vote_total})")
-print(f"Correy: {correy_pct}% ({correy_vote_total})")
-print(f"Li: {li_pct}% ({li_vote_total})")
-print(f"O'Tooley: {otooley_pct}% ({otooley_vote_total}){nl}-------------------------------")
+print(f"Kahn: {khan_pct}.000% ({khan_vote_total})")
+print(f"Correy: {correy_pct}.000% ({correy_vote_total})")
+print(f"Li: {li_pct}.000% ({li_vote_total})")
+print(f"O'Tooley: {otooley_pct}.000% ({otooley_vote_total}){nl}-------------------------------")
 print(f"Winner: {winner}{nl}-------------------------------")
 print(f"{nl}{nl}")
 
-# results_file = os.path.join('analysis', 'analysis.txt')
-# with open(results_file, 'a') as analysis_write:
-#     analysis_write.write(f"{nl}{nl}Election Results{nl}-------------------------------{nl}")
-#     analysis_write.write(f"Total Votes: {total_votes}{nl}-------------------------------{nl}")
-#     analysis_write.write(f"Kahn: {khan_pct}% ({khan_vote_total}){nl}")
-#     analysis_write.write(f"Correy: {correy_pct}% ({correy_vote_total}){nl}")
-#     analysis_write.write(f"Li: {li_pct}% ({li_vote_total}){nl}")
-#     analysis_write.write(f"O'Tooley: {otooley_pct}% ({otooley_vote_total}){nl}-------------------------------{nl}")
-#     analysis_write.write(f"Winner: {winner}{nl}-------------------------------{nl}")
-#     analysis_write.write(f"{nl}{nl}")
+results_file = os.path.join('analysis', 'analysis.txt')
+with open(results_file, 'a') as analysis_write:
+    analysis_write.write(f"{nl}{nl}Election Results{nl}-------------------------------{nl}")
+    analysis_write.write(f"Total Votes: {total_votes}{nl}-------------------------------{nl}")
+    analysis_write.write(f"Kahn: {khan_pct}.000% ({khan_vote_total}){nl}")
+    analysis_write.write(f"Correy: {correy_pct}.000% ({correy_vote_total}){nl}")
+    analysis_write.write(f"Li: {li_pct}.000% ({li_vote_total}){nl}")
+    analysis_write.write(f"O'Tooley: {otooley_pct}.000% ({otooley_vote_total}){nl}-------------------------------{nl}")
+    analysis_write.write(f"Winner: {winner}{nl}-------------------------------{nl}")
+    analysis_write.write(f"{nl}{nl}")
